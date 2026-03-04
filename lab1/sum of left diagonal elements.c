771177f9d56300874b0e6451f00970b01e1ba4c4
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2026-03-04 16:42:18 +0530

    Add files via upload

---
+++ lab1/sum of left diagonal elements.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    int sum=0;
+    printf("enter the size of array:");
+    scanf("%d",&n);
+    int arr[n][n];
+    printf("enter the elements:\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++){
+         sum=sum+arr[i][i];
+    }
+    printf("sum of left diagonal elements in an array is %d",sum);
+}
+
