118f68c1b50db8030249d9bb0a47f9d71bc899c3
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2026-03-05 17:12:29 +0530

    Add files via upload

---
+++ lab1/delete element in array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,pos,temp;
+    printf("enter number of elements in array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the elements\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("enter position:");
+    scanf("%d",&pos);
+    for(int i=pos-1;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    n--;
+    printf("array elements in the array\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
+
